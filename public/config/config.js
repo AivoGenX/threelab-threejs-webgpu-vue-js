9a7c234bf7d6d9e5a73397acb1105238abfcf32b
unknown <yanchao.chi@eulee.local>
2024-08-08 16:50:15 +0800

    bubble

--- public/config/config.js
+++ public/config/config.js
@@ -14,9 +14,12 @@ window.THREE_CESIUM_NAVIGATION = [
 window.THREE_CESIUM_AUTHORS = threeCesiumAuthors;
 
 /* 依赖注入 */
-window.GET_SCRIPT = (v, t) =>
-  (t === "Cesium.js案例[1.119]"
-    ? `<link rel="stylesheet" href="/public/js/cesium/style.css">
+window.GET_SCRIPT = (v, t) => {
+  let scriptContent = ``;
+
+  if (t === "Cesium.js案例[1.119]") {
+    scriptContent = `
+    <link rel="stylesheet" href="/public/js/cesium/style.css">
  <script type="importmap">
     {
         "imports": {
@@ -24,14 +27,29 @@ window.GET_SCRIPT = (v, t) =>
             "dat.gui": "/public/js/dat.gui.module.js"
         }
     }
- <\/script>`
-    : `    
-`) +
-  `  <link rel="stylesheet" type="text/css" href="/public/css/threelab.css" />   
-   
-    ${
-      t === "Cesium.js案例"
-        ? 'window.CESIUM_BASE_URL = "/public/cesium"'
-        : ""
-    }
-    ${v} `;
+ <\/script>
+      <style>
+        body {
+          margin: 0;
+          padding: 1px;
+          box-sizing: border-box;
+          background-color: #1f1f1f;
+          overflow: hidden;
+        }
+        #box {
+          width: 100%;
+          height: 100%;
+        }
+      </style>
+      <div id="box"></div>
+      <script type="module"> 
+        ${v}
+      </script>`;
+  } else {
+    scriptContent = `
+      <link rel="stylesheet" type="text/css" href="/public/css/threelab.css" /> 
+      ${v}`;
+  }
+
+  return scriptContent;
+};
